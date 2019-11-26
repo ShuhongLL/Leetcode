54dd781335d84bb10da836f8f14650caadd7c0b1
Shuhong Liu <ar-shuhong.liu@rakuten.com>
2019-11-26 17:56:17 +0900

    medium

---
+++ 692.top-k-frequent-words.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=692 lang=python3
+#
+# [692] Top K Frequent Words
+#
+
+# @lc code=start
+import itertools
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        words = sorted(words)
+        l = [(len(list(group)), key) for key, group in itertools.groupby(words)]
+        result =[item[1] for item in sorted(l, key = lambda x: [-x[0], x[1]])]
+        return result[:k]
+
+# @lc code=end
+
