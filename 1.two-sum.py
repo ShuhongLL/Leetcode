710863958f03819e267e572d2fc98833cbf6df61
Shuhong Liu <ar-shuhong.liu@rakuten.com>
2019-12-08 20:33:44 +0900

    easy

---
+++ 1.two-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = {}
+        for i in range(len(nums)):
+            if nums[i] in memo:
+                return [memo[nums[i]], i]
+            memo[target - nums[i]] = i
