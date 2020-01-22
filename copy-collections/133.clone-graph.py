371d818280ac421c78fd3ac5f4d3e052c81ca3f1
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2020-01-22 14:22:08 -0500

    add clone graph

---
+++ copy-collections/133.clone-graph.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = []):
+        self.val = val
+        self.neighbors = neighbors
+"""
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        seen = {}
+        queue = [node]
+        seen[node] = Node(node.val, [])
+        while queue:
+            n = queue.pop(0)
+            for neighbor in n.neighbors:
+                if neighbor not in seen:
+                    seen[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
+                seen[n].neighbors.append(seen[neighbor])
+        return seen[node]        
