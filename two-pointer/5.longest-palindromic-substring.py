dc880ab0f4ddc27027530177feb454a5bbe81d32
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2020-01-21 22:16:31 -0500

    lru

--- two-pointer/5.longest-palindromic-substring.py
+++ two-pointer/5.longest-palindromic-substring.py
@@ -7,10 +7,10 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1:
             return s 
-        curMax = maxStr = s[0]
+        cur = maxStr = s[0]
         for i in range(1, len(s)):
-            curMax = s[:i+1]
-            while curMax[::-1] != curMax:
-                curMax = curMax[1:]
-            maxStr = curMax if len(curMax) > len(maxStr) else maxStr
+            cur = s[:i+1]
+            while cur[::-1] != cur:
+                cur = cur[1:]
+            maxStr = cur if len(cur) > len(maxStr) else maxStr
         return maxStr
