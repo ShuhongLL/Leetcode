ecf8ff6c62aa879358ae31efad43a939c3ffec40
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2020-02-02 22:13:31 -0500

    append to greedy

--- greedy/659.split-array-into-consecutive-subsequences.py
+++ greedy/659.split-array-into-consecutive-subsequences.py
@@ -6,24 +6,30 @@
 
 # @lc code=start
 import collections
+
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         count = collections.Counter(nums)
         tail = collections.Counter()
         for num in nums:
+            # already consumed num in prev step
             if count[num] == 0:
                 continue
+            # prefer append to an existing list
             if tail[num] > 0:
                 tail[num] -= 1
-                tail[num+1] += 1
+                tail[num + 1] += 1
+            # no existing list ends as num - 1
+            # create a new list if valid
             elif count[num+1] > 0 and count[num+2] > 0:
                 count[num+1] -= 1
                 count[num+2] -= 1
                 tail[num+3] += 1
+            # invalid case
             else:
                 return False
             count[num] -= 1
         return True
-     
+
 # @lc code=end
 
