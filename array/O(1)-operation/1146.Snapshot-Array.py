c6de0b9272a40b0a88b260a6430e8ac25c9d17c8
OhYoooo <32889561+OhYoooo@users.noreply.github.com>
2020-09-10 22:55:49 -0400

    add 1146

---
+++ array/O(1)-operation/1146.Snapshot-Array.py
@@ -0,0 +1,22 @@
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.snaps = []
+        self.nums = {}
+
+    def set(self, index: int, val: int) -> None:
+        self.nums[index] = val
+
+    def snap(self) -> int:
+        self.snaps.append(self.nums.copy())
+        return len(self.snaps) - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        return self.snaps[snap_id][index] if index in self.snaps[snap_id] else 0
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
